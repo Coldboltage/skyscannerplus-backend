0140847fbe11937cf167a71026f145412d026a6f
unknown <alandreid@hotmail.co.uk>
2022-05-06 20:48:34 +0100

    adding this for now

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
     script : "./server/src/checker.js",
     watch: true,
     env: {
+      "PORT": 8000,
       "MONGODB_CONNECTION": "mongodb+srv://coldbolt-flightscanner:Dy7GkhKhfyYa9Ds@scannerplus.zgsmw.mongodb.net/scannerplus?retryWrites=true&w=majority",
     }
   }]
